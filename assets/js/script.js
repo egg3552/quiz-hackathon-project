ab35793f60d37180e256634cd62c623ae3c5d205
fspruce <f.spruce@proton.me>
2025-08-18 19:12:33 +0100

    Add functions with comments, Initialise first question with startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,44 @@ const questions = [
 ];
 
 //Global Variables
-let container = document.getElementById("quizCarousel");
-let userOptions = container.getElementsByTagName("label");
-let questionNumber = 1;
-let questionNumbers = [];
+let currentQuestion = 1; //Keeps track of which question number the user is on.
+let questionNumbers = []; //Keeps track of which questions are available from the questions array.
 
 //Wait until page has loaded before firing functions
 document.addEventListener("DOMContentLoaded", () => {
   startQuiz();
 });
+
+/**
+ * Initialises the first question for the quiz; creating an array to keep track of which questions have already been used.
+ */
+function startQuiz() {
+  //Creates an array of indexes matching up to the number of questions available in the questions array.
+  for (let i = 0; i < questions.length; i++){
+    questionNumbers.push(i);
+  }
+  createOptions();
+  displayQuestion(questionNumbers);  
+}
+
+/**
+ * Generates the HTML needed for displaying a question within the quiz carousel.
+ */
+function createOptions() {
+  
+}
+
+/**
+ * Uses the questionNumbers array to generate a random question from the questions array.
+ */
+function displayQuestion(questionNumbers) {
+
+}
+
+/**
+ * Currently used for debugging.
+ * Generates more questions when called.
+ */
+function nextQuestion() {
+
+}
