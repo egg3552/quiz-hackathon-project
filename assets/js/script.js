325a2eb042574d36094a8601cb2b262951393d6a
fspruce <f.spruce@proton.me>
2025-08-18 18:55:22 +0100

    Add questions to JS as an object array

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Tool Markup Language"],
+    answer: "Hyper Text Markup Language"
+  },
+  {
+    question: "Which CSS property is used to change the text color?",
+    options: ["text-color", "color", "font-color", "text-style"],
+    answer: "color"
+  },
+  {
+    question: "Which JavaScript method is used to select an element by its ID?",
+    options: ["getElementById()", "getElementsByClassName()", "selectElement()", "querySelector()"],
+    answer: "getElementById()"
+  },
+];
