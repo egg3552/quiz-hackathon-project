6754fd24def9f0444ba0b0197b66dd596fd4bd71
fspruce <f.spruce@proton.me>
2025-08-18 19:32:53 +0100

    Bug Fix: Changed re-used variable name currentQuestion to currentQuestionNumber

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -24,7 +24,7 @@ const questions = [
 
 //Global Variables
 const optionLetters = ["A) ", "B) ", "C) ", "D) "];
-let currentQuestion = 1; //Keeps track of which question number the user is on.
+let currentQuestionNumber = 1; //Keeps track of which question number the user is on.
 let questionNumbers = []; //Keeps track of which questions are available from the questions array.
 
 //Wait until page has loaded before firing functions
@@ -49,26 +49,26 @@ function startQuiz() {
  */
 function createOptions() {
   let options = `
-  <!-- Question ${currentQuestion} -->
-  <div id="question-${currentQuestion}" class="carousel-item active">
+  <!-- Question ${currentQuestionNumber} -->
+  <div id="question-${currentQuestionNumber}" class="carousel-item active">
     <div class="card-body text-start">
-      <h4 class="quiz-question-title text-center">Question ${currentQuestion}</h4>
-      <p id="question-${currentQuestion}-text" class="quiz-question-text text-center"></p>
+      <h4 class="quiz-question-title text-center">Question ${currentQuestionNumber}</h4>
+      <p id="question-${currentQuestionNumber}-text" class="quiz-question-text text-center"></p>
       <div class="quiz-form-check">
-        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestion}" id="q${currentQuestion}a" style="display: none;">
-        <label class="form-check-label quiz-form-check-label" for="q${currentQuestion}a"></label>
+        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestionNumber}" id="q${currentQuestionNumber}a" style="display: none;">
+        <label class="form-check-label quiz-form-check-label" for="q${currentQuestionNumber}a"></label>
       </div>
       <div class="quiz-form-check">
-        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestion}" id="q${currentQuestion}b" style="display: none;">
-        <label class="form-check-label quiz-form-check-label" for="q${currentQuestion}b"></label>
+        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestionNumber}" id="q${currentQuestionNumber}b" style="display: none;">
+        <label class="form-check-label quiz-form-check-label" for="q${currentQuestionNumber}b"></label>
       </div>
       <div class="quiz-form-check">
-        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestion}" id="q${currentQuestion}c" style="display: none;">
-        <label class="form-check-label quiz-form-check-label" for="q${currentQuestion}c"></label>
+        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestionNumber}" id="q${currentQuestionNumber}c" style="display: none;">
+        <label class="form-check-label quiz-form-check-label" for="q${currentQuestionNumber}c"></label>
       </div>
       <div class="quiz-form-check">
-        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestion}" id="q${currentQuestion}d" style="display: none;">
-        <label class="form-check-label quiz-form-check-label" for="q${currentQuestion}d"></label>
+        <input class="form-check-input quiz-form-check-input" type="radio" name="question${currentQuestionNumber}" id="q${currentQuestionNumber}d" style="display: none;">
+        <label class="form-check-label quiz-form-check-label" for="q${currentQuestionNumber}d"></label>
       </div>
     </div>
   </div>
@@ -81,14 +81,14 @@ function createOptions() {
  */
 function displayQuestion(questionNumbers) {
   const rand = Math.floor(Math.random() * questionNumbers.length); //Generates a random number between 0 and one less than the number of questions left for the user to complete; to be used as an index number.
-  const currentQuestion = questions[questionNumbers[rand]]; //Retrieves the corresponding question object from the questions array.
+  const currentQuestion = questions[rand]; //Retrieves the corresponding question object from the questions array.
   questionNumbers.splice(rand, 1); //removes this index value from the questionNumbers array, so not to allow the question to appear again.
-  document.getElementById(`question-${currentQuestion}-text`).innerText = currentQuestion.question; //Sets the question on the page to the corresponding question from the questions array.
-  const questionContainer = document.getElementById(`question-${currentQuestion}`); 
+  document.getElementById(`question-${currentQuestionNumber}-text`).innerText = currentQuestion.question; //Sets the question on the page to the corresponding question from the questions array.
+  const questionContainer = document.getElementById(`question-${currentQuestionNumber}`); 
   const questionOptions = questionContainer.getElementsByTagName("label");
   //For all options available within the currently selected questions object, set the corresponding option on the page to be a letter and then the option (e.g. "A) Option 1, B) Option 2 ...").
   for (let i = 0; i < currentQuestion.options.length; i++) {
-    questionOptions[i].innerText = optionLetters[i] + currentQuestion.options[i];
+    questionOptions[i].innerText = currentQuestion.options[i];
   }
   //Give all options on the page an event handler to allow for further testing on adding new questions.
   for (let i = 0; i < currentQuestion.options.length; i++) {
@@ -101,10 +101,10 @@ function displayQuestion(questionNumbers) {
  * Generates more questions when called.
  */
 function nextQuestion() {
-  console.log(currentQuestion, questionNumbers);
+  console.log(currentQuestionNumber, questionNumbers);
   if (questionNumbers.length !== 0) {
-    document.getElementById(`question-${currentQuestion}`).classList.remove("active");
-    currentQuestion++;
+    document.getElementById(`question-${currentQuestionNumber}`).classList.remove("active");
+    currentQuestionNumber++;
     createOptions();
     displayQuestion(questionNumbers);
   } else {
